ab727dd6e296940ea8999ccace8a0db3bc773dfc
kimhyerin0909 <khyerin0909@gmail.com>
2022-06-21 13:54:03 +0900

    FEAT :: follow velog's header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function Header() {
   return (
@@ -12,7 +13,7 @@ export default function Header() {
         <nav>
           <button className='header-item'><svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M4.069 13h-4.069v-2h4.069c-.041.328-.069.661-.069 1s.028.672.069 1zm3.034-7.312l-2.881-2.881-1.414 1.414 2.881 2.881c.411-.529.885-1.003 1.414-1.414zm11.209 1.414l2.881-2.881-1.414-1.414-2.881 2.881c.528.411 1.002.886 1.414 1.414zm-6.312-3.102c.339 0 .672.028 1 .069v-4.069h-2v4.069c.328-.041.661-.069 1-.069zm0 16c-.339 0-.672-.028-1-.069v4.069h2v-4.069c-.328.041-.661.069-1 .069zm7.931-9c.041.328.069.661.069 1s-.028.672-.069 1h4.069v-2h-4.069zm-3.033 7.312l2.88 2.88 1.415-1.414-2.88-2.88c-.412.528-.886 1.002-1.415 1.414zm-11.21-1.415l-2.88 2.88 1.414 1.414 2.88-2.88c-.528-.411-1.003-.885-1.414-1.414zm6.312-10.897c-3.314 0-6 2.686-6 6s2.686 6 6 6 6-2.686 6-6-2.686-6-6-6z"></path></svg></button>
           <button className='header-item'><svg width="17" height="17" viewBox="0 0 17 17"><path fillRule="evenodd" d="M13.66 7.36a6.3 6.3 0 1 1-12.598 0 6.3 6.3 0 0 1 12.598 0zm-1.73 5.772a7.36 7.36 0 1 1 1.201-1.201l3.636 3.635c.31.31.31.815 0 1.126l-.075.075a.796.796 0 0 1-1.126 0l-3.636-3.635z" clipRule="evenodd" fill="currentColor"></path></svg></button>
-          <button className='new-post-btn header-item'>새 글 작성</button>
+          <NavLink to="newpost" className="new-post-btn header-item">새 글 작성</NavLink>
           <div className='img-radius'>
             <img className='' src='https://velog.velcdn.com/images/kimhyerin0909/profile/1ebb853d-a45f-4f52-8ab4-a7f3e1a05a1c/social.jpeg' alt='profile-img'/>
           </div>
