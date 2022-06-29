4bdda98bcea04349f5f1f409c13a89bef5a9d8b9
kimhyerin0909 <khyerin0909@gmail.com>
2022-06-29 14:34:07 +0900

    Etc - add post list

--- src/components/Recent.js
+++ src/components/Recent.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './Header'
 import Select from './Select'
+import Tranding from './Tranding'
 
 export default function Recent() {
   return (
@@ -8,6 +9,7 @@ export default function Recent() {
         <Header />
         <Select />
         최신글
+        <Tranding />
     </div>
   )
 }
